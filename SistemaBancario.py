d23dc42b50e935857a66a281c7aac5e5d6fd9dfa
Francisco Rigon <franciscorigon@gmail.com>
2023-08-16 16:15:04 -0300

    Restante do programa

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -18,13 +18,47 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito: ")
+        valor = float(input("Informe o valor do depósito: "))
+        
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido")
 
     elif opcao == "s":
-        print("Sacar: ")
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou. Saldo indisponível")
+
+        elif excedeu_limite:
+            print("Operação falhou. Limite de saque máximo de R$ 500,00 por operação")
+
+        elif excedeu_saques:
+            print("Operação falhou. Limite máximo de saques (3) diários")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou. Valor informado inválido")
+            
 
     elif opcao == "e":
-        print("Extrato: ")
+        print("\n================= EXTRATO ================= ")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=========================================== ")
 
     elif opcao == "q":
         break
